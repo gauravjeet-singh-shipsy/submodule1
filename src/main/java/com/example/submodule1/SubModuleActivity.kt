cf1d710542925fcf4476f52ede69274aad805bf0
Gauraveet Singh <gauravjeet.singh@shipsy.io>
2021-10-22 11:02:21 +0530

    commit from parent app2

--- src/main/java/com/example/submodule1/SubModuleActivity.kt
+++ src/main/java/com/example/submodule1/SubModuleActivity.kt
@@ -7,5 +7,6 @@ class SubModuleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub_module)
+        //Hi from App 2
     }
 }
